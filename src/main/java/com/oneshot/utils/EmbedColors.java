fdff326c6600b9802df0d677c458f02829906e12
simoikavalko-eng <simo.ikavalko@gmail.com>
2026-07-17 19:30:28 +0300

    Add src/main/java/com/oneshot/utils/EmbedColors.java

---
+++ src/main/java/com/oneshot/utils/EmbedColors.java
@@ -0,0 +1,65 @@
+package com.oneshot.utils;
+
+import java.awt.*;
+
+public final class EmbedColors {
+    private EmbedColors() {}
+
+    // Reds
+    public static final Color MAROON = new Color(128, 0, 0);
+    public static final Color DARK_RED = new Color(139, 0, 0);
+    public static final Color BROWN = new Color(165, 42, 42);
+    public static final Color FIREBRICK = new Color(178, 34, 34);
+    public static final Color CRIMSON = new Color(220, 20, 60);
+    public static final Color RED = new Color(255, 0, 0);
+    public static final Color TOMATO = new Color(255, 99, 71);
+    public static final Color CORAL = new Color(255, 127, 80);
+    public static final Color INDIAN_RED = new Color(205, 92, 92);
+    public static final Color LIGHT_CORAL = new Color(240, 128, 128);
+
+    // Oranges and gold
+    public static final Color ORANGE_RED = new Color(255, 69, 0);
+    public static final Color DARK_ORANGE = new Color(255, 140, 0);
+    public static final Color ORANGE = new Color(255, 165, 0);
+    public static final Color GOLD = new Color(255, 215, 0);
+
+    // Yellows
+    public static final Color YELLOW = new Color(255, 255, 0);
+    public static final Color YELLOW_GREEN = new Color(154, 205, 50);
+    public static final Color NEON_YELLOW = new Color(255,231,0);
+
+    // Greens
+    public static final Color DARK_GREEN = new Color(0, 100, 0);
+    public static final Color GREEN = new Color(0, 128, 0);
+    public static final Color FOREST_GREEN = new Color(34, 139, 34);
+    public static final Color LIME = new Color(0, 255, 0);
+    public static final Color LIGHT_GREEN = new Color(144, 238, 144);
+    public static final Color NEON_GREEN = new Color(116,238,21);
+
+    // Blues & Cyans
+    public static final Color TEAL = new Color(0, 128, 128);
+    public static final Color AQUA = new Color(0, 255, 255);
+    public static final Color TURQUOISE = new Color(64, 224, 208);
+    public static final Color DODGER_BLUE = new Color(30, 144, 255);
+    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color ROYAL_BLUE = new Color(65, 105, 225);
+    public static final Color NAVY = new Color(0, 0, 128);
+    public static final Color NEON_BLUE = new Color(77,238,234);
+
+    // Purples
+    public static final Color PURPLE = new Color(128, 0, 128);
+    public static final Color DARK_VIOLET = new Color(148, 0, 211);
+    public static final Color MAGENTA = new Color(255, 0, 255);
+
+    // Pinks
+    public static final Color HOT_PINK = new Color(255, 105, 180);
+    public static final Color DEEP_PINK = new Color(255, 20, 147);
+    public static final Color NEON_PINK = new Color(255,19,240);
+
+    // Neutrals
+    public static final Color WHITE = new Color(255, 255, 255);
+    public static final Color LIGHT_GRAY = new Color(211, 211, 211);
+    public static final Color GRAY = new Color(128, 128, 128);
+    public static final Color DARK_GRAY = new Color(169, 169, 169);
+    public static final Color BLACK = new Color(0, 0, 0);
+}
